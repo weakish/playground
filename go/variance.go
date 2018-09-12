ce65254f3985a199d08c64700088d243b73e55af
Jakukyo Friel <weakish@gmail.com>
2018-09-12 21:03:49 +0800

    go: no support for covariant return type

--- go/variance.go
+++ go/variance.go
@@ -6,13 +6,23 @@ func h(sup func(int) interface{}, x int) {
 	sup(x)
 }
 
+// subtyping of function, contravariant argument, and convariant result
 func sub(x interface{}) int {
 	fmt.Println(x)
 	return 1
 }
 
+
+// Java support this.
+func covariantReturn(x int) int {
+	fmt.Println(x)
+	return 3
+}
+
 func main() {
 	h(sub, 2)
 	// cannot use sub (type func(interface {}) int) as type func(int) interface {} in argument to h
+	h(covariantReturn, 4)
+	// cannot use covariantReturn (type func(int) int) as type func(int) interface {} in argument to h
 }
 
