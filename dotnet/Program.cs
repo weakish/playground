e68e7f0a2ca85bed051e1df28f6b135722fb0947
Jang Rush <weakish@gmail.com>
2019-01-29 15:25:22 +0800

    c#: hello world

---
+++ dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using LeanCloud;
+
+namespace csharp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            AVClient.Initialize("r7WOBCoFgBlpuRoBTIWjb7XI-gzGzoHsz", "PT4FpSvABfO0wKCD4WYA0wdK");
+
+            AVObject football = new AVObject("Sport");
+            football["totalTime"] = 90;
+            football["name"] = "Football";
+            Task saveTask = football.SaveAsync();
+            saveTask.Wait();
+        }
+    }
+}
