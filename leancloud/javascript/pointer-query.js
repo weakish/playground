b786448382e5e52e904d4e29f4fcf743ee3b8ed9
Jang Rush <weakish@gmail.com>
2019-03-05 14:37:22 +0800

    :new: (leancloud) pointer query

---
+++ leancloud/javascript/pointer-query.js
@@ -0,0 +1,19 @@
+var APP_ID = 'jYDw5C3mQvCJ0Us6yOVT8De8-gzGzoHsz';
+var APP_KEY = 'fwj014f87mhTGAqQxOfNkcU6';
+var AV = require('leancloud-storage');
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+const user = AV.Object.createWithoutData('_User', '5c7e0ea1fe88c200653f85e0')
+var query = new AV.Query('posts')
+query = query.equalTo('author', user)
+console.log("QUERY ...")
+console.log(query)
+var first = query.first()
+console.log("FIRST ...")
+console.log(first)
+console.log("PROMISE")
+first.then((post) => { console.log(post.content) }, (error) => { console.log("error") } )
