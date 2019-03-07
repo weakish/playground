9087426df931b99184d76a12ba54e78b0344ac64
Jang Rush <weakish@gmail.com>
2019-03-07 23:54:18 +0800

    :fix: (leancloud) java example

--- leancloud/java/lean-demo/src/main/java/page/mmap/lean/demo/App.java
+++ leancloud/java/lean-demo/src/main/java/page/mmap/lean/demo/App.java
@@ -1,13 +1,65 @@
 package page.mmap.lean.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import cn.leancloud.AVObject;
+import cn.leancloud.core.AVOSCloud;
+
+public class App {
+    public static void main(String[] args) {
+        AVOSCloud.initialize("jYDw5C3mQvCJ0Us6yOVT8De8-gzGzoHsz", "fwj014f87mhTGAqQxOfNkcU6");
+        AVObject fromJava = new AVObject("FromJava");
+        fromJava.put("hello", "Java");
+        fromJava.put("empty", null);
+        fromJava.save();
+//        AVQuery<AVObject> avQuery = new AVQuery<>("TestObject");
+//        avQuery.getInBackground("5c45436a7565716f24d459ae").subscribe(new Observer<AVObject>() {
+//            @Override
+//            public void onSubscribe(Disposable disposable) { }
+//
+//            @Override
+//            public void onNext(AVObject avObject) {
+//                String hello = avObject.getString("hello");
+//                System.out.printf("hello: %s\n", hello);
+//                String nonExist = avObject.getString("nonExist");
+//                System.out.printf("nonExist: %s\n", nonExist);
+//                String empty = avObject.getString("empty");
+//                System.out.printf("empty: %s", empty);
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
+
+//        AVObject fromJava = AVObject.createWithoutData("FromJava", "5c4aaf5844d904004d6359a0");
+//        fromJava.fetchInBackground().subscribe(new Observer<AVObject>() {
+//            @Override
+//            public void onSubscribe(Disposable disposable) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(AVObject avObject) {
+//                String hello = avObject.getString("hello");
+//                System.out.printf("hello: %s", hello);
+//                avObject.put("hello", null);
+//                avObject.save();
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
     }
 }
