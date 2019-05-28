06870122c4da068d834d690ce8255661b77c2eb0
Jang Rush <weakish@gmail.com>
2019-05-28 14:11:35 +0800

    :new:leancloud: python test updatedKeys

--- leancloud/python/mysql/cloud.py
+++ leancloud/python/mysql/cloud.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import leancloud
 from leancloud import Engine
 from leancloud import LeanEngineError
 
@@ -13,6 +14,11 @@ def hello(**params):
     if 'name' in params:
         return 'Hello, {}!'.format(params['name'])
     else:
+        leancloud.use_production(os.environ['LEANCLOUD_APP_ENV'] == 'production')
+        leancloud.init("3AJTBW5QyGw17hLxNN3njGHW-gzGzoHsz", "P8cKdEvRxusfuR27MuhvFOzr")
+        todo = leancloud.Object.extend('Todo')()
+        todo.set('content', '')
+        todo.save()
         return app_env
 
 
@@ -20,6 +26,32 @@ def hello(**params):
 def before_todo_save(todo):
     content = todo.get('content')
     if not content:
-        raise LeanEngineError('内容不能为空')
+        todo.set('content', 'stage')
     if len(content) >= 240:
         todo.set('content', content[:240] + ' ...')
+
+@engine.before_update('_User')
+def check_email(user):
+    if 'email' not in user.updated_keys:
+        return
+    if "@example.com" not in user.get('email'):
+        raise leancloud.LeanEngineError(message=('暂不支持该 Email 服务提供商'))
+
+
+@engine.before_update('AComment')
+def likes_must_not_be_negative(a_comment):
+    if 'likes' not in a_comment.get('_updatedKeys'):
+        return
+    if a_comment.get('likes') < 0:
+        raise leancloud.LeanEngineError(message=('likes 属性不支持负数'))
+
+
+"""
+@engine.after_update('AComment')
+def likes_must_not_be_negative(a_comment):
+    if 'likes' not in a_comment.updated_keys:
+        return
+    if a_comment.get('likes') < 0:
+        a_comment.set('likes', 0)
+        a_comment.save()
+"""
--- leancloud/python/mysql/wsgi.py
+++ leancloud/python/mysql/wsgi.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 from gevent import monkey
 monkey.patch_all()
 
@@ -17,7 +21,7 @@ PORT = int(os.environ['LEANCLOUD_APP_PORT'])
 
 leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 # 如果需要使用 master key 权限访问 LeanCLoud 服务，请将这里设置为 True
-leancloud.use_master_key(False)
+leancloud.use_master_key(True)
 
 # 需要重定向到 HTTPS 可去除下一行的注释。
 # app = leancloud.HttpsRedirectMiddleware(app)
