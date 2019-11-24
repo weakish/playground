052adb606d304f5e451e6de14099a6013e6e1dc6
Jang Rush <weakish@gmail.com>
2019-11-24 15:02:35 +0800

    :art:leancloud:java cleanup

--- leancloud/java/forum-20357/src/main/java/page/mmap/lean/demo/App.java
+++ leancloud/java/forum-20357/src/main/java/page/mmap/lean/demo/App.java
@@ -1,4 +1,14 @@
-package main.java.page.mmap.lean.demo;
+package page.mmap.lean.demo;
+
+import cn.leancloud.AVObject;
+import cn.leancloud.AVQuery;
+import cn.leancloud.core.AVOSCloud;
 
 public class App {
+    public static void main(String[] args) {
+        AVOSCloud.initialize("3AJTBW5QyGw17hLxNN3njGHW-gzGzoHsz", "P8cKdEvRxusfuR27MuhvFOzr");
+        AVQuery<AVObject> avQuery = new AVQuery<>("Post");
+        AVObject avObject = avQuery.get("5c88d968303f390037d1152e");
+        System.out.println(avObject.getObjectId() == "");
+    }
 }
--- leancloud/java/lean-demo/src/main/java/page/mmap/lean/demo/App.java
+++ leancloud/java/lean-demo/src/main/java/page/mmap/lean/demo/App.java
@@ -1,28 +1,18 @@
 package page.mmap.lean.demo;
 
-import cn.leancloud.AVLogger;
 import cn.leancloud.AVObject;
+import cn.leancloud.AVQuery;
 import cn.leancloud.core.AVOSCloud;
-
-import java.util.Arrays;
-
 
 
 public class App {
     public static void main(String[] args) {
         AVObject.registerSubclass(Travel.class);
         AVObject.registerSubclass(Form.class);
-        AVOSCloud.initialize("jYDw5C3mQvCJ0Us6yOVT8De8-gzGzoHsz", "fwj014f87mhTGAqQxOfNkcU6");
-        AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
-//        AVObject fromJava = new AVObject("FromJava");
-//        fromJava.put("hello", "Java");
-//        fromJava.put("empty", null);
-//        fromJava.save();
-        Travel travel = new Travel();
-        Form form1 = new Form();
-        Form form2 = new Form();
-        Form form3 = new Form();
-        travel.addAll("forms", Arrays.asList(form1, form2, form3));
-        travel.save();
+        AVOSCloud.initialize("3AJTBW5QyGw17hLxNN3njGHW-gzGzoHsz", "P8cKdEvRxusfuR27MuhvFOzr");
+
+        AVQuery<AVObject> avQuery = new AVQuery<>("Post");
+        AVObject avObject = avQuery.get("5c88d968303f390037d1152e");
+        System.out.println(avObject.getObjectId() == "");
     }
 }
