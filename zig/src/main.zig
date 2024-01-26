72a477bfd308e1796d61ceb5af5aec9e8f64c0f0
weakish <weakish@gmail.com>
2024-01-26 13:26:28 +0000

    :new:zig: struct default value

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -3,26 +3,18 @@ const print = std.debug.print;
 
 /// A structure for storing a timestamp, with nanosecond precision (this is a
 /// multiline doc comment).
+// cSpell:words struct
 const Timestamp = struct {
-    /// The number of seconds since the epoch (this is also a doc comment).
-    seconds: i64, // signed so we can represent pre-1970 (not a doc comment)
-    /// The number of nanoseconds past the second (doc comment again).
-    nanos: u32,
-
-    /// Returns a `Timestamp` struct representing the Unix epoch; that is, the
-    /// moment of 1970 Jan 1 00:00:00 UTC (this is a doc comment too).
-    pub fn unixEpoch() Timestamp {
-        return Timestamp{
-            .seconds = 0,
-            .nanos = 0,
-        };
-    }
+    seconds: i64 = 0, // signed so we can represent pre-1970 (not a doc comment)
+    nanoSeconds: u32 = 0,
 };
 
 pub fn main() !void {
     print("Hi, ", .{});
     const stdout = std.io.getStdOut();
     _ = try stdout.write("there!\n");
-    const epoch = Timestamp.unixEpoch();
-    print("Unix Epoch: {}", .{epoch.seconds});
+    const epoch = Timestamp{};
+    print("Unix Epoch: {}.{}\n", .{ epoch.seconds, epoch.nanoSeconds });
+    const hhaeee = 2;
+    print("Haeee, {}", .{hhaeee});
 }
