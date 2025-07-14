1bcbb3cb8fae0f29e56b3e275cbd1ac367c729a0
weakish <weakish@gmail.com>
2025-07-14 18:46:13 +0000

    :new: test remark-obsidian under bun

---
+++ bun/README.md
@@ -0,0 +1,16 @@
+# bun
+
+To install dependencies:
+
+```bash
+bun install
+cd node_modules/remark-obsidian
+npm install
+npm run build
+```
+
+To run:
+
+```bash
+bun run 
+```
--- bun/hello.ts
+++ bun/hello.ts
@@ -1,3 +1,14 @@
-const a: string = 'a'
+import { unified } from 'unified'
+import remarkObsidian from 'remark-obsidian';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
 
-console.log(a)
+const { value } = unified()
+    .use(remarkParse)
+    .use(remarkObsidian, { baseUrl: '/docs' })
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .processSync('[[Hello world]]');
+
+console.log(value); // <a href="/docs/hello-world">Hello world</a>
