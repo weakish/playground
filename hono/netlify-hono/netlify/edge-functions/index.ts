389d0e22f82949ab5041d207fc936553c7aed68d
weakish <weakish@gmail.com>
2025-07-29 08:04:50 +0000

    :new: netlify deno

---
+++ hono/netlify-hono/netlify/edge-functions/index.ts
@@ -0,0 +1,10 @@
+import { Hono } from 'jsr:@hono/hono'
+import { handle } from 'jsr:@hono/hono/netlify'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default handle(app)
